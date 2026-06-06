cdcce5a84e4efbde7554b266ec195d038ad9e7bc
qrafaelfaria <rrfaria2020@gmail.com>
2026-06-06 20:07:15 -0300

    dqwe

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -31,9 +31,9 @@ import FemBonus from "@/assets/FemBonus.png";
 import OneBonus from "@/assets/100Bonus.png";
 
 const CHECKOUT_URL = "#checkout";
-const PREMIUM_CHECKOUT_URL = "https://pay.wiapy.com/6a03c77a7483096845580587";
-const BASIC_CHECKOUT_URL = "https://pay.wiapy.com/sVm2xI1lE";
-const DISCOUNTED_PREMIUM_CHECKOUT_URL = "https://pay.wiapy.com/g1NogNsgbN"; // URL com desconto para R$ 14,90 (Upsell)
+const PREMIUM_CHECKOUT_URL = "https://pay.wiapy.com/ulk84ywIP_";
+const BASIC_CHECKOUT_URL = "https://pay.wiapy.com/de77OJCli";
+const DISCOUNTED_PREMIUM_CHECKOUT_URL = "https://pay.wiapy.com/jAzDNswYn5"; // URL com desconto para R$ 14,90 (Upsell)
 
 const useCountdown = (minutes: number) => {
   const target = useMemo(() => Date.now() + minutes * 60 * 1000, [minutes]);
